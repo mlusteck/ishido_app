9c1377fce7abc24f56f5a6bfbb96aa62166c5015
Mischa Lusteck <mlusteck@googlemail.com>
2018-07-29 12:43:18 +0200

    4.7 Add pulsating four-way indicator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,12 @@
 //= require turbolinks
 //= require jquery3
 //= require_tree .
+function toFull(){
+  $(".four-way").animate({opacity: '1.0'}, 1000, "swing", toDark );
+}
+function toDark(){
+  $(".four-way").animate({opacity: '0.7'}, 800, "swing", toFull );
+}
 
 $(document).ready( function () {
   $(".place-stone").click( function () {
@@ -24,4 +30,8 @@ $(document).ready( function () {
     $("#board_y").attr("value", board_y );
     $(".hidden-form").submit();
   })
-})
+
+  $(".four-way").animate({opacity: '1.0'}, 100, "swing", toDark );
+
+  // color animation for four-way indicator
+  });
