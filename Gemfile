846446f3f821f98eb509e3471ef4a1371b390891
Mischa Lusteck <mlusteck@googlemail.com>
2018-08-08 17:58:51 +0200

    5.11 Configure email

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'jquery-rails'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'figaro'
+
 # Authentication
 gem 'devise'
 
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+# SMTP settings for gmail
+ActionMailer::Base.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV['gmail_username'],
+ :password             => ENV['gmail_password'],
+ :authentication       => "plain",
+ :enable_starttls_auto => true
+}
