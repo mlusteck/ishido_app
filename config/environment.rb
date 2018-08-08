d59d1362de73d774e455641d6108267a5b4f027c
Mischa Lusteck <mlusteck@googlemail.com>
2018-08-08 18:34:40 +0200

    5.11 Configure email

--- config/environment.rb
+++ config/environment.rb
@@ -8,11 +8,20 @@ Rails.application.initialize!
 ActionMailer::Base.delivery_method = :smtp
 
 # SMTP settings for gmail
+#ActionMailer::Base.smtp_settings = {
+# :address              => "smtp.gmail.com",
+# :port                 => 587,
+# :user_name            => ENV['gmail_username'],
+# :password             => ENV['gmail_password'],
+# :authentication       => "plain",
+# :enable_starttls_auto => true
+#}
+#
 ActionMailer::Base.smtp_settings = {
- :address              => "smtp.gmail.com",
- :port                 => 587,
- :user_name            => ENV['gmail_username'],
- :password             => ENV['gmail_password'],
+ :address              => "smtp.mischa-lusteck.de",
+ :port                 => 25,
+ :user_name            => ENV['ml_username'],
+ :password             => ENV['ml_password'],
  :authentication       => "plain",
  :enable_starttls_auto => true
 }
